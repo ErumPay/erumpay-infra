b0304394ec5d4e46279848f9a24f125ac225bf78
Yeongeunn <nye0817@gmail.com>
2026-05-12 09:41:36 +0900

    chore: 로컬 인프라 레포 초기 구성 추가

---
+++ mysql/init/00-create-databases.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE IF NOT EXISTS auth_db
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_unicode_ci;
+
+CREATE DATABASE IF NOT EXISTS card_db
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_unicode_ci;
+
+CREATE DATABASE IF NOT EXISTS payment_db
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_unicode_ci;
+
+CREATE DATABASE IF NOT EXISTS recommend_db
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_unicode_ci;
+
+CREATE DATABASE IF NOT EXISTS notification_db
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_unicode_ci;
+
+CREATE DATABASE IF NOT EXISTS pg_auth_db
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_unicode_ci;
+
+CREATE DATABASE IF NOT EXISTS pg_billing_db
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_unicode_ci;
+
+CREATE DATABASE IF NOT EXISTS pg_payment_db
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_unicode_ci;
+
+CREATE DATABASE IF NOT EXISTS merchant_db
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_unicode_ci;
+
+CREATE DATABASE IF NOT EXISTS card_simulator_db
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_unicode_ci;
+
+GRANT ALL PRIVILEGES ON auth_db.* TO 'erumpay'@'%';
+GRANT ALL PRIVILEGES ON card_db.* TO 'erumpay'@'%';
+GRANT ALL PRIVILEGES ON payment_db.* TO 'erumpay'@'%';
+GRANT ALL PRIVILEGES ON recommend_db.* TO 'erumpay'@'%';
+GRANT ALL PRIVILEGES ON notification_db.* TO 'erumpay'@'%';
+GRANT ALL PRIVILEGES ON pg_auth_db.* TO 'erumpay'@'%';
+GRANT ALL PRIVILEGES ON pg_billing_db.* TO 'erumpay'@'%';
+GRANT ALL PRIVILEGES ON pg_payment_db.* TO 'erumpay'@'%';
+GRANT ALL PRIVILEGES ON merchant_db.* TO 'erumpay'@'%';
+GRANT ALL PRIVILEGES ON card_simulator_db.* TO 'erumpay'@'%';
+
+FLUSH PRIVILEGES;
