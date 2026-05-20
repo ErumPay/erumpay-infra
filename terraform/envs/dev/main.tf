80e1e4b3c5377db6661f08368c0afcfa8d5d67e9
Yeongeunn <nye0817@gmail.com>
2026-05-20 19:57:34 +0900

    [KAN-1094] feat: Security Group 모듈 구현 (EKS 클러스터/노드/RDS)

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -5,3 +5,9 @@ module "vpc" {
   private_subnets = var.private_subnets
   azs             = var.azs
 }
+
+# vpc 모듈에서 만든 vpc_id를 security 모듈에 전달
+module "security" {
+  source = "../../modules/security"
+  vpc_id = module.vpc.vpc_id
+}
